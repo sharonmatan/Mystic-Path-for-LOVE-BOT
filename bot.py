44d6245d31f1d3ec7dfde78a7b8e2b05470c2c4f
hagitram <ram1hagit@gmail.com>
2020-01-26 21:34:47 +0200

    stage 1.1 still need work

--- bot.py
+++ bot.py
@@ -1,10 +1,55 @@
+import logging
+import model
+from telegram import Update
+from telegram.ext import CommandHandler, CallbackContext, MessageHandler, \
+    Filters, Updater
+
 import secrets
+
+BASE = "https://en.wikipedia.org/wiki/"
+
+logging.basicConfig(
+    format='[%(levelname)s %(asctime)s %(module)s:%(lineno)d] %(message)s',
+    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+
+def start(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    logger.info(f"> Start chat #{chat_id}")
+    name = update.effective_user['first_name']
+    print(name)
+    text = model.get_behind_name(name)
+    context.bot.send_message(chat_id=chat_id, text=f"Hello {name}! you have a wonderful name.. did you know that:")
+    context.bot.send_message(chat_id=chat_id, text=f"{text}")
+
+
+def respond(update: Update, context: CallbackContext):
+    chat_id = update.effective_chat.id
+    text = update.message.text
+    logger.info(f"= Got on chat #{chat_id}: {text!r}")
+    response = model.get_behind_name(text)
+    context.bot.send_message(chat_id=update.message.chat_id, text=response)
 
 
 def main():
     print(secrets.BOT_TOKEN)
     # YOUR BOT HERE
+    updater = Updater(token=secrets.BOT_TOKEN, use_context=True)
+    dispatcher = updater.dispatcher
+    start_handler = CommandHandler('start', start)
+    dispatcher.add_handler(start_handler)
+
+    echo_handler = MessageHandler(Filters.text, respond)
+    dispatcher.add_handler(echo_handler)
+
+    logger.info("* Start polling...")
+    updater.start_polling()  # Starts polling in a background thread.
+    updater.idle()  # Wait until Ctrl+C is pressed
+    logger.info("* Bye!")
 
 
 if __name__ == '__main__':
     main()
+
--- model.py
+++ model.py
@@ -1,1 +1,16 @@
 # YOUR BOT LOGIC/STORAGE/BACKEND FUNCTIONS HERE
+import requests
+
+
+def get_behind_name(name: str):
+    r = requests.get(f"https://www.behindthename.com/name/{name}")
+    print(r.headers)
+    start_index = r.text.find("Meaning & History")
+    print(start_index)
+    end_index = r.text.find("</div>", start_index+33)
+    print(end_index)
+    # print(start_index, end_index)
+    age = r.text[start_index+35:end_index] if start_index != -1 else '0'
+    # print(age, type(age))
+    msg = age if age != '0' else f"Sorry, I don't know any '{name}'."
+    return msg
